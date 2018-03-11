4e2acd0489ac5d03b03b934f3ad650cbfcbc387f
wujunmin <wujm14@fudan.edu.cn>
2018-03-11 12:00:29 +0800

    add disease_info crawler

--- medscrawler/spiders/disease_info.py
+++ medscrawler/spiders/disease_info.py
@@ -6,20 +6,20 @@ import random
 import scrapy
 
 
-class DiseaseDetailSpider(scrapy.Spider):
-    name = "disease_detail"
+class DiseaseInfoSpider(scrapy.Spider):
+    name = "disease_info"
     host = 'http://ypk.39.net'
     referer = 'http://ypk.39.net/allcategory'
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        with open('disease.json') as disease_file:
+        with open('disease_detail.json') as disease_file:
             self.data = json.load(disease_file)
 
         self.crawled = set()
         try:
-            with open('disease_detail.json') as disease_detail_file:
-                crawled_data = json.load(disease_detail_file)
+            with open('disease_info.json') as disease_info_file:
+                crawled_data = json.load(disease_info_file)
                 for l_ in crawled_data:
                     for name in l_:
                         self.crawled.add(name)
@@ -71,33 +71,40 @@ class DiseaseDetailSpider(scrapy.Spider):
 
     def start_requests(self):
         for d_ in self.data:
-            for category in d_:
-                for disease, url in d_[category].items():
-                    if disease in self.crawled:
-                        continue
-                    yield scrapy.Request(url=url, callback=self.parse, headers={
-                        'Referer': self.referer,
-                        'User-Agent': random.choice(self.USER_AGENTS),
-                    })
+            for disease in d_:
+                if disease in self.crawled:
+                    continue
+                url = d_[disease]['info']
+                yield scrapy.Request(url=url, callback=self.parse, headers={
+                    'Referer': self.referer,
+                    'User-Agent': random.choice(self.USER_AGENTS),
+                })
 
     def parse(self, response):
-        name = response.css('h2.label_title::text').extract_first()
-        info = response.css('h2.label_title a::attr(href)').extract_first()
-        med_path = response.css('div.label_ypsz_box .content p.name a::attr(href)').extract()
-        med_name = response.css('div.label_ypsz_box .content p.name a::text').extract()
+        disease = response.css('dl.intro dt::text').extract_first()
+        raw = response.css('.info ul li > *:not(cite)::text ,.info ul li::text').extract()
+        trimmed = [r_.strip() for r_ in raw if r_.strip()]
+
+        # 解析 infobox
+        parsed = {}
+        i = 0
+        while i < len(trimmed):
+            c_ = trimmed[i]
+            if c_.endswith('：'):
+                l_ = []
+                i += 1
+                while i < len(trimmed) and not trimmed[i].endswith('：'):
+                    l_.append(trimmed[i])
+                    i += 1
+                parsed[c_[:-1]] = l_ if len(l_) > 1 else l_.pop()
 
         # 防止被反爬虫
-        if not med_name:
+        if not trimmed:
             yield scrapy.Request(url=response.url, callback=self.parse, dont_filter=True, headers={
                 'Referer': self.referer,
                 'User-Agent': random.choice(self.USER_AGENTS),
             })
         else:
             yield {
-                name: {
-                    'info': info,
-                    'meds': {
-                        n: self.host + p for n, p in zip(med_name, med_path)
-                    }
-                }
+                disease: parsed
             }
