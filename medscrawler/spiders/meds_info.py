52da2aaf8f43e4a5e498ab7fc72ec3f428466f8b
wujunmin <wujm14@fudan.edu.cn>
2018-03-11 15:38:18 +0800

    change json file to jl

--- medscrawler/spiders/meds_info.py
+++ medscrawler/spiders/meds_info.py
@@ -7,8 +7,8 @@ import scrapy
 from scrapy.utils.project import get_project_settings
 
 
-class DiseaseInfoSpider(scrapy.Spider):
-    name = "disease_info"
+class MedsInfoSpider(scrapy.Spider):
+    name = "meds_info"
     host = 'http://ypk.39.net'
     referer = 'http://ypk.39.net/allcategory'
     USER_AGENTS = get_project_settings().get('USER_AGENTS')
@@ -22,8 +22,8 @@ class DiseaseInfoSpider(scrapy.Spider):
 
         self.crawled = set()
         try:
-            with open('disease_info.jl') as file:
-                for line in file:
+            with open('meds_info.jl') as meds_info_file:
+                for line in meds_info_file:
                     crawled_data = json.loads(line)
                     for name in crawled_data:
                         self.crawled.add(name)
@@ -35,32 +35,38 @@ class DiseaseInfoSpider(scrapy.Spider):
     def start_requests(self):
         for d_ in self.data:
             for disease in d_:
-                if disease in self.crawled:
-                    continue
-                self.crawled.add(disease)
-                url = d_[disease]['info']
-                yield scrapy.Request(url=url, callback=self.parse, headers={
-                    'Referer': self.referer,
-                    'User-Agent': random.choice(self.USER_AGENTS),
-                })
+                meds = d_[disease]['meds']
+                for m_, u_ in meds.items():
+                    if m_ in self.crawled:
+                        continue
+                    self.crawled.add(m_)
+                    yield scrapy.Request(url=u_ + 'manual', callback=self.parse, headers={
+                        'Referer': self.referer,
+                        'User-Agent': random.choice(self.USER_AGENTS),
+                    })
 
     def parse(self, response):
-        disease = response.css('dl.intro dt::text').extract_first()
-        raw = response.css('.info ul li > *:not(cite)::text ,.info ul li::text').extract()
-        trimmed = [r_.strip() for r_ in raw if r_.strip()]
+        med = response.css('.t1 h1 a::text').extract_first()
+        raw = response.css('div.tab_box div dl dt::text, div.tab_box div dl dd::text,'
+                           ' div.tab_box div dl p::text, div.tab_box div dl a::text').extract()
+
+        # 适应症特殊处理
+        trimmed = [r_.strip() if not '适应症' in r_.strip() else '【适应症】' for r_ in raw if r_.strip()]
 
         # 解析 infobox
         parsed = {}
         i = 0
         while i < len(trimmed):
             c_ = trimmed[i]
-            if c_.endswith('：'):
+            if c_.endswith('】'):
                 l_ = []
                 i += 1
-                while i < len(trimmed) and not trimmed[i].endswith('：'):
+                while i < len(trimmed) and not trimmed[i].endswith('】'):
                     l_.append(trimmed[i])
                     i += 1
-                parsed[c_[:-1]] = l_ if len(l_) > 1 else l_.pop()
+                parsed[c_[1:-1]] = "\n".join(l_)
+
+        print(parsed)
 
         # 防止被反爬虫
         if not trimmed:
@@ -70,5 +76,5 @@ class DiseaseInfoSpider(scrapy.Spider):
             })
         else:
             yield {
-                disease: parsed
+                med: parsed
             }
